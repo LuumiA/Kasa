5a6d7f8fbb22fd2226009cc27089fda8e07cd98c
LuumiA <nicolas.cava13@gmail.com>
2025-02-07 20:12:20 +0100

    Création du composant NotFound

---
+++ src/pages/notFound/NotFound.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.scss";
+
+const NotFound: React.FC = () => {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="home-link">
+        Retourner sur la page d'accueil
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
